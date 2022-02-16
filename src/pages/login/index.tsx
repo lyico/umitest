b3a0500ea2bded46e5d4f212fd9f80947b0078b4
beta_king <931835103@qq.com>
2022-02-16 17:43:41 +0800

    test

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,22 +3,33 @@
  * @Author       : chenLiang
  * @Date         : 2022-02-11 17:29:40
  * @LastEditors  : chenLiang
- * @LastEditTime : 2022-02-11 17:51:12
+ * @LastEditTime : 2022-02-16 10:27:22
  */
 
-import { Form, Input, Button } from "antd";
+import { createContext } from "react";
 
-interface  User {
+import { Form, Input, Button } from "antd";
+import styles from './index.less'
+
+import {ClInput, TestRate} from '../../components/ClInput'
+// import {$http} from '../../global/$http';
+
+
+
+// $http.get('test/123')
+
+// $http.get('123',{}, {showErr: false})
+interface User {
     usename: string;
     password: string;
-}
+} 
 
 const LoginPage = () => {
 
-    const onFinish = (values: User) => {
+    const onFinish = (values: {[key: string]: string}) => {
         console.log("success: ", values);
-        Object.keys(values).forEach((key: string) => {
-            // console.log(values[item]);
+        Object.keys(values).forEach((key) => {
+            console.log(values[key]);
         })
     }
 
@@ -26,9 +37,14 @@ const LoginPage = () => {
         console.log('Failed:', error);
     }
 
+    const onCheckedChange = (flag: boolean) =>{
+        console.log(flag);
+    }
 
-    return <div className="login-page-box">
-        <Form className="login-form-box" onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
+    return <div className={styles.login_page_box}>
+        <TestRate value={1} beta={'123'} count={1} onChange={num=> onCheckedChange(!!num)} disabled/>
+        <ClInput cc={123} placeholder="4156"/>
+        <Form labelCol={{ span: 4 }} className={styles.login_form_box} onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
             <Form.Item label="用户名" name="usename" rules={[{ required: true, message: '请输入用户名' }]}>
                 <Input />
             </Form.Item>
@@ -36,7 +52,7 @@ const LoginPage = () => {
                 <Input.Password />
             </Form.Item>
             <Form.Item>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" block size="large">
                     提交
                 </Button>
             </Form.Item>
